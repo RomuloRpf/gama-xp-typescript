51489ad23cc22ede3d4500d8a866de4450d5532d
Romulo Peigas <rpf.2008@yahoo.com.br>
2021-07-09 12:11:27 -0300

    Typescript || 06 - Type Alias

--- aula3/tipos.ts
+++ aula3/tipos.ts
@@ -73,3 +73,28 @@ function exibirNota(nota: number | string) {
 
 exibirNota('10');
 exibirNota(10);
+
+// Alias
+type Funcionario = {
+    nome: string;
+    sobrenome: string;
+    dataNascimento: Date;
+}
+
+//type Funcionarios = Array<Funcionario>;
+const funcionarios: Funcionario[] = [{
+    nome: 'Fulano',
+    sobrenome: 'De tal',
+    dataNascimento: new Date()
+},
+{
+    nome: 'Ciclano',
+    sobrenome: 'Silva',
+    dataNascimento: new Date()
+}];
+
+function tratarFuncionarios(funcionarios: Funcionario[]){
+    for(let funcionario of funcionarios){
+        console.log('Nome do funcionário: ', funcionario.nome);
+    }
+}
