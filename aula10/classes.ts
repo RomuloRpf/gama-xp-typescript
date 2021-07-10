69535e75d2e3c7b3d8b471518ca73ddb740230ef
Romulo Peigas <rpf.2008@yahoo.com.br>
2021-07-10 14:23:03 -0300

    Typescript || 12 - Utilizando Modificadores de Acesso

--- aula10/classes.ts
+++ aula10/classes.ts
@@ -14,3 +14,31 @@ const data = new Data(1,1,2020);
 console.log(data.dia);
 
 const data2 = new Data(1,1);
+
+class Carro {
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMaxima: number = 220
+    ) {}
+
+    alterarVelocidade(delta: number) {
+        if(this.velocidadeAtual + delta <= this.velocidadeMaxima){
+            this.velocidadeAtual += delta;
+        }
+    }
+
+    acelerar() {
+        this.alterarVelocidade(5);
+    }
+
+    frear(){
+        const frenagem = -5;
+        this.velocidadeAtual -= frenagem;
+    }
+}
+
+const carro = new Carro('Ford','Camaro',300);
+carro.acelerar();
